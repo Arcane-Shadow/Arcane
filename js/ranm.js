62d279f598d2c63140f17a441a342d1f79f9256a
Arcane-Shadow <135852225+Arcane-Shadow@users.noreply.github.com>
2023-07-02 10:48:58 +0530

    Update ranm.js

--- js/ranm.js
+++ js/ranm.js
@@ -29,10 +29,10 @@ function tabulateAnswers() {
       answerbox.innerHTML = "You're working really hard in order to increase ur typing speed, but u need to follow some typing rules and etiquetts for better improvement, posture/equipment like monitor,keyboard etc positon should be proper and focus on consistancy and accuracy for better results!";
     }
     if (c2score == maxscore) {
-      answerbox.innerHTML = "YOu're naturally gifted with good typing abilities and improving a lot, u just need to work on consistancy and good posture aligning with ur eye sight and comfortable psotion of keybaord for hand, wish u a good luck!";
+      answerbox.innerHTML = "You're naturally gifted with good typing abilities and improving a lot, u just need to work on consistancy and good posture aligning with ur eye sight and comfortable psotion of keybaord for hand, wish u a good luck!";
     }
     if (c3score == maxscore) { 
-      answerbox.innerHTML = "nice job! You're already at good start and just need to focus on how to make typing more productive!";
+      answerbox.innerHTML = "nice job! You're already at a good start and just need to focus on how to make typing more productive!";
     }
     if (c4score == maxscore) { 
       answerbox.innerHTML = "You need to work very hard! Use the correct starting position,Don’t look down your hands maintain a good posture find a comfortable position for your hands and just practice a lot!";
